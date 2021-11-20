8110fa60fb0b7a346ccae230151edcaec398ca1a
Milad Mazaheri <milad.kbr@outlook.com>
2021-11-20 10:12:38 +0330

    SetStartup

--- EsiApiClient/App.xaml.cs
+++ EsiApiClient/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using EsiApiClient.Models;
+using Microsoft.Win32;
 
 namespace EsiApiClient
 {
@@ -18,5 +19,26 @@ namespace EsiApiClient
         public static ConfigModel AppConfig { get; set; }
         public static string ConfigFilePath = Directory.GetCurrentDirectory() + @"\config.json";
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SetStartup();
+        }
+        private void SetStartup()
+        {
+            var appName = "IPA_Client";
+            var exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName;
+
+            if (exePath != null)
+            {
+                ////Current User
+                //RegistryKey rku = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                //rku.SetValue(appName, exePath);
+
+                ////Local Machine
+                RegistryKey rkm = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                rkm.SetValue(appName, exePath);
+            }
+        }
     }
 }
