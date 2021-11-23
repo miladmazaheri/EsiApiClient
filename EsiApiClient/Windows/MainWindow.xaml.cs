19b6d68d85add96d433808843689e807605aae63
Milad Mazaheri <milad.kbr@outlook.com>
2021-11-23 16:22:04 +0330

    1

--- EsiApiClient/Windows/MainWindow.xaml.cs
+++ EsiApiClient/Windows/MainWindow.xaml.cs
@@ -224,6 +224,43 @@ namespace IPAClient.Windows
             lblDate.Content = now.ToString("HH:mm") + " " + now.ToPersianDateTextify();
         }
 
+        private Reservation MapToReservation(MainInfo_Send_Offline_Data_Fun_Output_Data input)
+        {
+            return new Reservation
+            {
+                Id = Guid.NewGuid(),
+                Date = DateTime.Now.ToString(),
+                Cod_Meal = null,
+                Date_Use = null,
+                Status = null,
+                Time_Use = null,
+
+                Main_Course = input.Main_Course.Select(x => new Food { Des_Food = x.Des_Food, Num_Amount = x.Num_Amount, Typ_Serv_Unit = x.Typ_Serv_Unit }).ToList(),
+                Appetizer_Dessert = input.Appetizer_Dessert.Select(x => new Food { Des_Food = x.Des_Food, Num_Amount = x.Num_Amount, Typ_Serv_Unit = x.Typ_Serv_Unit }).ToList(),
+                Cod_Contract_Order = input.Cod_Contract_Order,
+                Cod_Coupon = input.Cod_Coupon,
+                Cod_Resturant = input.Cod_Resturant,
+                Cod_Serial = input.Cod_Serial,
+                Dat_Day_Mepdy = input.Dat_Day_Mepdy,
+                Des_Contract_Order = input.Des_Contract_Order,
+                Des_Food_Order_Mepdy = input.Des_Food_Order_Mepdy,
+                Des_Nam_Meal = input.Des_Nam_Meal,
+                Des_Nam_Resturant_Rstm = input.Des_Nam_Resturant_Rstm,
+                Employee_Shift_Name = input.Employee_Shift_Name,
+                First_Name_Ide = input.First_Name_Ide,
+                Last_Name_Ide = input.Last_Name_Ide,
+                Lkp_Cod_Order_Mepdy_Means = input.Lkp_Cod_Order_Mepdy_Means,
+                Meal_Plan_Day_Id = input.Meal_Plan_Day_Id,
+                Num_Ide = input.Num_Ide,
+                Num_Tim_End_Meal_Rsmls = input.Num_Tim_End_Meal_Rsmls,
+                Num_Tim_Str_Meal_Rsmls = input.Num_Tim_Str_Meal_Rsmls,
+                Num_Tot_Coupon_Rccpn = input.Num_Tot_Coupon_Rccpn,
+                Receiver_Meal_Plan_Day_Id = input.Receiver_Meal_Plan_Day_Id,
+                Reciver_Coupon_Id = input.Reciver_Coupon_Id,
+
+            };
+        }
+
         #region ReConfig Listener
         private void InitReConfigListener()
         {
@@ -348,42 +385,18 @@ namespace IPAClient.Windows
             }
         }
         #endregion
-        //map MainInfo_Send_Offline_Data_Fun_Output_Data to reservation
-        private Reservation MapToReservation(MainInfo_Send_Offline_Data_Fun_Output_Data input)
+        
+        private async Task CheckReservation(string personnelNumber)
         {
-            return new Reservation
+            if (App.AppConfig.CheckOnline)
             {
-                Id = Guid.NewGuid(),
-                Date = DateTime.Now.ToString(),
-                Cod_Meal = null,
-                Date_Use = null,
-                Status = null,
-                Time_Use = null,
-
-                Main_Course = input.Main_Course.Select(x => new Food { Des_Food = x.Des_Food, Num_Amount = x.Num_Amount, Typ_Serv_Unit = x.Typ_Serv_Unit }).ToList(),
-                Appetizer_Dessert = input.Appetizer_Dessert.Select(x => new Food { Des_Food = x.Des_Food, Num_Amount = x.Num_Amount, Typ_Serv_Unit = x.Typ_Serv_Unit }).ToList(),
-                Cod_Contract_Order = input.Cod_Contract_Order,
-                Cod_Coupon = input.Cod_Coupon,
-                Cod_Resturant = input.Cod_Resturant,
-                Cod_Serial = input.Cod_Serial,
-                Dat_Day_Mepdy = input.Dat_Day_Mepdy,
-                Des_Contract_Order = input.Des_Contract_Order,
-                Des_Food_Order_Mepdy = input.Des_Food_Order_Mepdy,
-                Des_Nam_Meal = input.Des_Nam_Meal,
-                Des_Nam_Resturant_Rstm = input.Des_Nam_Resturant_Rstm,
-                Employee_Shift_Name = input.Employee_Shift_Name,
-                First_Name_Ide = input.First_Name_Ide,
-                Last_Name_Ide = input.Last_Name_Ide,
-                Lkp_Cod_Order_Mepdy_Means = input.Lkp_Cod_Order_Mepdy_Means,
-                Meal_Plan_Day_Id = input.Meal_Plan_Day_Id,
-                Num_Ide = input.Num_Ide,
-                Num_Tim_End_Meal_Rsmls = input.Num_Tim_End_Meal_Rsmls,
-                Num_Tim_Str_Meal_Rsmls = input.Num_Tim_Str_Meal_Rsmls,
-                Num_Tot_Coupon_Rccpn = input.Num_Tot_Coupon_Rccpn,
-                Receiver_Meal_Plan_Day_Id = input.Receiver_Meal_Plan_Day_Id,
-                Reciver_Coupon_Id = input.Reciver_Coupon_Id,
-
-            };
+                var res = await ApiClient.Restrn_Queue_Have_Reserve_Fun(new RESTRN_QUEUE_HAVE_RESERVE_FUN_Input());
+                if(res != null)
+                {
+                    res.
+                }
+            }
         }
+        
     }
 }
