7cefd46899a16ff2192f25d2eeca3b9b4cf00fda
miladmazaheri <milad.kbr@outlook.com>
2021-12-11 23:54:21 +0330

    EpsilonBackgroundService

---
+++ EpsilonBackgroundService/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace EpsilonService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool _IsInstalled = false;
+            bool serviceStarting = false;
+            string SERVICE_NAME = "EpsilonBackgroundService";
+
+            ServiceController[] services = ServiceController.GetServices();
+
+            foreach (ServiceController service in services)
+            {
+                if (service.ServiceName.Equals(SERVICE_NAME))
+                {
+                    _IsInstalled = true;
+                    if (service.Status == ServiceControllerStatus.StartPending)
+                    {
+                        // If the status is StartPending then the service was started via the SCM             
+                        serviceStarting = true;
+                    }
+                    break;
+                }
+            }
+            if (!serviceStarting)
+            {
+                if (_IsInstalled == true)
+                {
+                    if (args.Length != 0)
+                    {
+                        if (args[0].ToLower() == "u")
+                        {
+                            SelfInstaller.UninstallMe();
+                        }
+                    }
+                }
+                else
+                {
+
+                    SelfInstaller.InstallMe();
+                    ServiceController controller = new ServiceController(SERVICE_NAME);
+                    if (controller.Status == ServiceControllerStatus.Running)
+                        controller.Stop();
+
+                    if (controller.Status == ServiceControllerStatus.Stopped)
+                        controller.Start();
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new EpsilonService()
+                };
+                ServiceBase.Run(services: ServicesToRun);
+            }
+        }
+
+    }
+}
