270a7f68dbbcb66f9ab0371fefa64080bdfd4869
miladmazaheri <milad.kbr@outlook.com>
2021-12-20 01:24:23 +0330

    Epsilon Runner Service

---
+++ EpsilonRunnerService/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EpsilonRunnerService
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService(options =>
+                {
+                    options.ServiceName = "Epsilon Runner Service";
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ EpsilonRunnerService/Worker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using EpsilonService;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Timer = System.Timers.Timer;
+
+namespace EpsilonRunnerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private static readonly string CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private readonly string _applicationName = CurrentDirectory + "HandBrake";//"IPAClient.exe";
+        private Timer timer;
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Epsilon Runner Service Started", DateTimeOffset.Now);
+            timer = new Timer { Interval = 1000 * 10 }; // تنظیم فاصله زمانی بمدت ‍‍‍‍‍‍‍15 تانیه
+            timer.Elapsed += TimerOnElapsed;
+            timer.Enabled = true;
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            RunServer();
+        }
+
+        private void RunServer()
+        {
+            try
+            {
+                if (!Process.GetProcesses().Any(p => p.ProcessName.Contains("IPAClient")))
+                {
+                    ApplicationLoader.PROCESS_INFORMATION procInfo;
+                    bool result = ApplicationLoader.StartProcessAndBypassUAC(_applicationName, CurrentDirectory, out procInfo);
+                    _logger.LogInformation(result ? "Epsilon Client Started" : "Epsilon Client Not Started", DateTimeOffset.Now);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error On Starting IPAClient", DateTimeOffset.Now);
+            }
+
+        }
+    }
+}
